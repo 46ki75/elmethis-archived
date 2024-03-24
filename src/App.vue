af7147dc950cceeacb64e36bcaa6415c649b0f73
shirayuki <shirayuki@46ki75.com>
2024-03-25 00:46:10 +0900

    chore(styles): introduce SCSS preprocessor

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
@@ -11,10 +9,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .logo {
   height: 6em;
   padding: 1.5em;
