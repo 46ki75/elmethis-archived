cf8d7856a7831a9355cf2cb91838026137321843
shirayuki <shirayuki@46ki75.com>
2024-03-25 23:59:51 +0900

    feat(CodeBlock): create CodeBlock component

---
+++ src/components/code/CodeBlock.stories.ts
@@ -0,0 +1,88 @@
+import { Meta, StoryObj } from '@storybook/vue3'
+
+import CodeBlock from './CodeBlock.vue'
+
+export default {
+  component: CodeBlock,
+  title: 'Code/CodeBlock',
+  // tags: ['autodocs'],
+  argTypes: {}
+} as Meta
+
+type Story = StoryObj<typeof CodeBlock>
+
+const code = `
+use reqwest::Error;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Post {
+    #[serde(rename = "userId")]
+    user_id: u32,
+    id: u32,
+    title: String,
+    body: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let url = "https://jsonplaceholder.typicode.com/posts";
+    let response = reqwest::get(url).await?;
+    let posts: Vec<Post> = response.json().await?;
+
+    for post in posts.iter().take(5) {
+        println!("ID: {}, Title: {}", post.id, post.title);
+    }
+
+    Ok(())
+}
+`
+
+export const Primary: Story = {
+  args: {
+    code,
+    language: 'rust',
+    theme: 'light'
+  }
+}
+
+export const Dark: Story = {
+  args: {
+    code,
+    language: 'rust',
+    theme: 'dark'
+  }
+}
+
+const tsCode = `
+import { createHash } from 'crypto';
+
+export function generateSHA256Hash(input: string): string {
+  return createHash('sha256').update(input).digest('hex');
+}
+`
+
+export const OtherLanguage: Story = {
+  args: {
+    code: tsCode,
+    language: 'typescript',
+    theme: 'light'
+  }
+}
+
+export const WithCaption: Story = {
+  args: {
+    code,
+    language: 'rust',
+    caption: 'src/main.rs'
+  }
+}
+
+export const Loading: Story = {
+  args: {
+    code,
+    language: 'rust',
+    caption: 'src/main.rs',
+    isLoading: true
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
+
+import 'prismjs'
+import './assets/prism-one-dark.css'
+// import './assets/prism-one-light.css'
+
 import './style.css'
 import App from './App.vue'
 
