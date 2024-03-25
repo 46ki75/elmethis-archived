7570d2c1a43b9e26d68b5c4b52504cebb71b68b3
shirayuki <shirayuki@46ki75.com>
2024-03-26 02:02:42 +0900

    feat(breadcrumbs): create breadcrumb component

---
+++ src/components/navigation/Breadcrumb.stories.ts
@@ -0,0 +1,35 @@
+import { Meta, StoryObj } from '@storybook/vue3'
+
+import Breadcrumb from './Breadcrumb.vue'
+
+export default {
+  component: Breadcrumb,
+  title: 'Navigation/Breadcrumb',
+  tags: ['autodocs'],
+  argTypes: {}
+} as Meta
+
+type Story = StoryObj<typeof Breadcrumb>
+
+export const Primary: Story = {
+  args: {
+    links: [
+      { href: '', label: 'Home' },
+      { href: '', label: 'Blog' },
+      { href: '', label: 'Article' }
+    ]
+  }
+}
+
+export const Deep: Story = {
+  args: {
+    links: [
+      { href: '', label: 'Home' },
+      { href: '', label: 'Blog' },
+      { href: '', label: 'Category' },
+      { href: '', label: 'Diary' },
+      { href: '', label: 'Archived' },
+      { href: '', label: 'Article' }
+    ]
+  }
+}
