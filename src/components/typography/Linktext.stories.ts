69a480a9deaedfd0e0ebc72fa4a6d3646bb505f5
shirayuki <shirayuki@46ki75.com>
2024-03-26 12:15:41 +0900

    feat: create Linktext component

---
+++ src/components/typography/Linktext.stories.ts
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import Linktext from './Linktext.vue'
+
+const meta: Meta<typeof Linktext> = {
+  component: Linktext,
+  title: 'Typography/Linktext',
+  tags: ['autodocs'],
+  argTypes: {
+    external: { options: 'toggle' }
+  }
+}
+
+export default meta
+type Story = StoryObj<typeof Linktext>
+
+export const Primary: Story = {
+  args: {
+    text: 'Link Text',
+    href: '',
+    external: false
+  }
+}
+
+export const External: Story = {
+  args: {
+    text: 'Link Text (External)',
+    href: 'https://example.com',
+    external: true
+  }
+}
