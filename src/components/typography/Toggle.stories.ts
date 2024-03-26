5a71a31f6f46662f84f7a79d77fd06e0f474d3b4
shirayuki <shirayuki@46ki75.com>
2024-03-26 13:17:42 +0900

    create Toggle Component

---
+++ src/components/typography/Toggle.stories.ts
@@ -0,0 +1,80 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import Toggle from './Toggle.vue'
+import CodeBlock from '../code/CodeBlock.vue'
+import { ref } from 'vue'
+
+const meta: Meta<typeof Toggle> = {
+  component: Toggle,
+  title: 'Typography/Toggle',
+  tags: ['autodocs'],
+  argTypes: {}
+}
+
+export default meta
+type Story = StoryObj<typeof Toggle>
+
+const Template = (args: any) => ({
+  components: { Toggle },
+  setup() {
+    return { args }
+  },
+  template: `
+  <Toggle summary="ToggleBlock">
+    <div>
+      It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </div>
+  </Toggle>
+  `
+})
+
+export const Primary: Story = {
+  render: Template,
+  args: {
+    summary: 'Equation'
+  }
+}
+
+const code = ref(`use reqwest::Error;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Post {
+    #[serde(rename = "userId")]
+    user_id: u32,
+    id: u32,
+    title: String,
+    body: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let url = "https://jsonplaceholder.typicode.com/posts";
+    let response = reqwest::get(url).await?;
+    let posts: Vec<Post> = response.json().await?;
+
+    for post in posts.iter().take(5) {
+        println!("ID: {}, Title: {}", post.id, post.title);
+    }
+
+    Ok(())
+}`)
+
+const TemplateCode = () => ({
+  components: { Toggle, CodeBlock },
+  setup() {
+    return { code }
+  },
+  template: `
+    <Toggle summary="Code">
+      <CodeBlock :code="code" language="rust" />
+    </Toggle>
+  `
+})
+
+export const Code: Story = {
+  render: TemplateCode,
+  args: {
+    summary: 'Code'
+  }
+}
---
+++ src/components/typography/Toggle.vue
@@ -0,0 +1,139 @@
+<template>
+  <details @click="setIsOpen(!isOpen)">
+    <summary
+      class="summary"
+      :style="{ borderRadius: isOpen ? '0.25rem 0.25rem 0 0' : '0.25rem' }"
+    >
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="12"
+          height="12"
+          opacity="0.6"
+          viewBox="0 0 352 512"
+          :style="{
+            marginRight: '0.5rem',
+            transition: 'all 0.2s',
+            transform: `rotateZ(${isOpen ? '180deg' : '45deg'})`
+          }"
+        >
+          <path
+            d="M242.7 256l100.1-100.1c12.3-12.3 12.3-32.2 0-44.5l-22.2-22.2c-12.3-12.3-32.2-12.3-44.5 0L176 189.3 75.9 89.2c-12.3-12.3-32.2-12.3-44.5 0L9.2 111.5c-12.3 12.3-12.3 32.2 0 44.5L109.3 256 9.2 356.1c-12.3 12.3-12.3 32.2 0 44.5l22.2 22.2c12.3 12.3 32.2 12.3 44.5 0L176 322.7l100.1 100.1c12.3 12.3 32.2 12.3 44.5 0l22.2-22.2c12.3-12.3 12.3-32.2 0-44.5L242.7 256z"
+            fill="darkgreen"
+          />
+        </svg>
+        <span>{{ summary }}</span>
+      </div>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="12"
+        viewBox="0 0 448 512"
+        :style="{
+          transform: `rotateX(${isOpen ? '180deg' : '0deg'})`,
+          transition: 'all 0.2s'
+        }"
+      >
+        <path
+          d="M207 381.5L12.7 187.1c-9.4-9.4-9.4-24.6 0-33.9l22.7-22.7c9.4-9.4 24.5-9.4 33.9 0L224 284.5l154.7-154c9.4-9.3 24.5-9.3 33.9 0l22.7 22.7c9.4 9.4 9.4 24.6 0 33.9L241 381.5c-9.4 9.4-24.6 9.4-33.9 0z"
+        />
+      </svg>
+    </summary>
+  </details>
+  <div
+    class="children"
+    :style="{
+      transition: `all 0.2s ${isOpen ? '0s' : '0.2s'}`,
+      transform: `scaleY(${isOpen ? 1 : 0})`,
+      height: `${isOpen ? '100%' : 0}`
+    }"
+  >
+    <div
+      :style="{
+        transition: `opacity 0.2s ${isOpen ? '0.2s' : '0s'}`,
+        opacity: `${isOpen ? 1 : 0}`
+      }"
+    >
+      <slot />
+    </div>
+  </div>
+  <div
+    class="fold"
+    :style="{ opacity: isOpen ? 0.7 : 0 }"
+    @click="setIsOpen(false)"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="12"
+      height="12"
+      viewBox="0 0 352 512"
+    >
+      <path
+        d="M242.7 256l100.1-100.1c12.3-12.3 12.3-32.2 0-44.5l-22.2-22.2c-12.3-12.3-32.2-12.3-44.5 0L176 189.3 75.9 89.2c-12.3-12.3-32.2-12.3-44.5 0L9.2 111.5c-12.3 12.3-12.3 32.2 0 44.5L109.3 256 9.2 356.1c-12.3 12.3-12.3 32.2 0 44.5l22.2 22.2c12.3 12.3 32.2 12.3 44.5 0L176 322.7l100.1 100.1c12.3 12.3 32.2 12.3 44.5 0l22.2-22.2c12.3-12.3 12.3-32.2 0-44.5L242.7 256z"
+      />
+    </svg>
+    <span>FOLD</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isOpen = ref<boolean>(false)
+const setIsOpen = (flag: boolean) => {
+  isOpen.value = flag
+}
+
+defineProps<{
+  /**
+   * **required!**
+   *
+   * The string that is displayed on the header of a collapsible tab.
+   * It is always visible, whether the tab is collapsed or not.
+   */
+  summary: string
+}>()
+</script>
+
+<style scoped>
+.summary {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  background-color: rgba(0, 0, 0, 0.02);
+
+  cursor: pointer;
+  user-select: none;
+  padding: 0.75rem;
+  border: solid 1px rgba(0, 0, 0, 0.25);
+}
+
+.children {
+  padding: 0.75rem;
+  border: solid 1px rgba(0, 0, 0, 0.25);
+  border-top: none;
+
+  border-radius: 0 0 0.25rem 0.25rem;
+
+  transform-origin: 50% 0%;
+
+  /* div {
+    transition: opacity 0.2s ${isOpen ? '0.2s' : '0s'};
+    opacity: ${isOpen ? 1 : 0};
+  } */
+}
+
+.fold {
+  padding: 0.5rem 0;
+  transition: all 0.1s;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 0.25rem;
+
+  cursor: pointer;
+}
+</style>
