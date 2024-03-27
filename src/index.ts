c21aa6379e1a72bbd621afd6af50f492cc62ce6d
shirayuki <shirayuki@46ki75.com>
2024-03-27 09:38:20 +0900

    feat(index): add bulk export for npm publication

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+// code
+export * from './components/code/CodeBlock.vue'
+
+// form
+export * from './components/form/Checkbox.vue'
+
+// icons
+export * from './components/icons/ConnectionIcon.vue'
+export * from './components/icons/GridLoadingIcon.vue'
+export * from './components/icons/InfoIcon.vue'
+export * from './components/icons/TriangleLoadingIcon.vue'
+
+// navigation
+export * from './components/navigation/Breadcrumb.vue'
+export * from './components/navigation/Pagetop.vue'
+
+// notion
+export * from './components/notion/NotionHTML.vue'
+export * from './components/notion/NotionRichText.vue'
+
+// typography
+export * from './components/typography/BookmarkCard.vue'
+export * from './components/typography/Callout.vue'
+export * from './components/typography/Divider.vue'
+export * from './components/typography/FragmentIdentifier.vue'
+export * from './components/typography/Heading1.vue'
+export * from './components/typography/Heading2.vue'
+export * from './components/typography/Heading3.vue'
+export * from './components/typography/ImageWithModal.vue'
+export * from './components/typography/Linktext.vue'
+export * from './components/typography/Toggle.vue'
+
+// utils
+export * from './components/utils/AnimateInView.vue'
