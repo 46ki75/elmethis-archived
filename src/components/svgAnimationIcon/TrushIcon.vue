65046e830ea43894563b4e37f50cbc1afe57fcc6
shirayuki <shirayuki@46ki75.com>
2024-06-29 03:37:29 +0900

    feat: Create modal component

---
+++ src/components/svgAnimationIcon/TrushIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <object
+    :style="{ width: `${size}px` }"
+    type="image/svg+xml"
+    :data="svgPath"
+  ></object>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const svgPath = computed(
+  () => new URL('@/assets/trush.svg', import.meta.url).href
+)
+
+withDefaults(
+  defineProps<{
+    /**
+     * **optional?**
+     *
+     * Size of the Icon.
+     */
+    size?: number
+  }>(),
+  { size: 64 }
+)
+</script>
+
+<style scoped lang="scss"></style>
