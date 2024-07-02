354cb1d09fcce52f49285f842a778c997a9f6242
shirayuki <shirayuki@46ki75.com>
2024-07-02 21:30:31 +0900

    docs: Update documentation comments in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,44 @@
+// # --------------------------------------------------------------------------------
+//
 // Code
+//
+// # --------------------------------------------------------------------------------
+
 export { default as CodeBlock } from './components/Code/CodeBlock/CodeBlock.vue'
 
-// Containments
+// # --------------------------------------------------------------------------------
+//
+// Containment
+//
+// # --------------------------------------------------------------------------------
+
 export { default as Modal } from './components/Containment/Modal/Modal.vue'
 export { default as Toggle } from './components/Containment/Toggle/Toggle.vue'
 export { default as Tooltip } from './components/Containment/Tooltip/Tooltip.vue'
 
+// # --------------------------------------------------------------------------------
+//
 // Data
+//
+// # --------------------------------------------------------------------------------
+
 export { default as LinearProgress } from './components/Data/LinearProgress/LinearProgress.vue'
 
+// # --------------------------------------------------------------------------------
+//
 // Form
+//
+// # --------------------------------------------------------------------------------
+
 export { default as Checkbox } from './components/Form/Checkbox/Checkbox.vue'
 export { default as SwitchTheme } from './components/Form/SwitchTheme/SwitchTheme.vue'
 
+// # --------------------------------------------------------------------------------
+//
 // Icon
+//
+// # --------------------------------------------------------------------------------
+
 export { default as ConnectionIcon } from './components/Icons/ConnectionIcon/ConnectionIcon.vue'
 export { default as DotLoadingIcon } from './components/Icons/DotLoadingIcon/DotLoadingIcon.vue'
 export { default as GridLoadingIcon } from './components/Icons/GridLoadingIcon/GridLoadingIcon.vue'
@@ -23,23 +48,48 @@ export { default as SquareLoadingIcon } from './components/Icons/SquareLoadingIc
 export { default as TriangleLoadingIcon } from './components/Icons/TriangleLoadingIcon/TriangleLoadingIcon.vue'
 export { default as TrushIcon } from './components/Icons/TrushIcon/TrushIcon.vue'
 
+// # --------------------------------------------------------------------------------
+//
 // Image
+//
+// # --------------------------------------------------------------------------------
+
 export { default as ImageWithFallback } from './components/Image/ImageWithFallback/ImageWithFallback.vue'
 export { default as ImageWithFallbackSmall } from './components/Image/ImageWithFallbackSmall/ImageWithFallbackSmall.vue'
 export { default as ImageWithModal } from './components/Image/ImageWithModal/ImageWithModal.vue'
 
+// # --------------------------------------------------------------------------------
+//
 // Navigation
+//
+// # --------------------------------------------------------------------------------
+
 export { default as Breadcrumb } from './components/Navigation/Breadcrumb/Breadcrumb.vue'
 export { default as Pagetop } from './components/Navigation/Pagetop/Pagetop.vue'
 
+// # --------------------------------------------------------------------------------
+//
 // Notion
+//
+// # --------------------------------------------------------------------------------
+
 export { default as NotionHTML } from './components/Notion/NotionHTML.vue'
 export { default as NotionRichText } from './components/Notion/NotionRichText.vue'
 
+// # --------------------------------------------------------------------------------
+//
 // Text
+//
+// # --------------------------------------------------------------------------------
+
 export { default as TurnText } from './components/Text/TurnText/TurnText.vue'
 
+// # --------------------------------------------------------------------------------
+//
 // Typography
+//
+// # --------------------------------------------------------------------------------
+
 export { default as BookmarkCard } from './components/Typography/BookmarkCard/BookmarkCard.vue'
 export { default as Callout } from './components/Typography/Callout/Callout.vue'
 export { default as Divider } from './components/Typography/Divider/Divider.vue'
@@ -50,5 +100,10 @@ export { default as Heading2 } from './components/Typography/Heading2/Heading2.v
 export { default as Heading3 } from './components/Typography/Heading3/Heading3.vue'
 export { default as Linktext } from './components/Typography/Linktext/Linktext.vue'
 
+// # --------------------------------------------------------------------------------
+//
 // Utils
+//
+// # --------------------------------------------------------------------------------
+
 export { default as AnimateInView } from './components/Utils/AnimateInView/AnimateInView.vue'
