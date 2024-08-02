b2181bc3ffea1d626261cfc80905200325157005
shirayuki <shirayuki@46ki75.com>
2024-08-02 11:14:51 +0900

    refactor: Rewrite animation of Toggle component using transitions

--- src/components/Containment/Toggle/Toggle.vue
+++ src/components/Containment/Toggle/Toggle.vue
@@ -40,45 +40,30 @@
       </svg>
     </summary>
   </details>
-  <div
-    class="children"
-    :style="{
-      marginBottom: margin ?? 0,
-      transition: `all 0.2s ${isOpen ? '0s' : '0.2s'}`,
-      transform: `scaleY(${isOpen ? 1 : 0})`,
-      height: `${isOpen ? '100%' : 0}`
-    }"
-  >
-    <div
-      :style="{
-        transition: `opacity 0.2s ${isOpen ? '0.2s' : '0s'}`,
-        opacity: `${isOpen ? 1 : 0}`
-      }"
-    >
-      <slot />
+
+  <transition name="children">
+    <div v-if="isOpen" class="children">
+      <div>
+        <slot />
+      </div>
     </div>
-  </div>
-  <div
-    class="fold"
-    :style="{
-      opacity: isOpen ? 0.7 : 0,
-      transition: `all 0.2s ${isOpen ? '0s' : '0.2s'}`,
-      height: `${isOpen ? '100%' : 0}`
-    }"
-    @click="setIsOpen(false)"
-  >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="12"
-      height="12"
-      viewBox="0 0 352 512"
-    >
-      <path
-        d="M242.7 256l100.1-100.1c12.3-12.3 12.3-32.2 0-44.5l-22.2-22.2c-12.3-12.3-32.2-12.3-44.5 0L176 189.3 75.9 89.2c-12.3-12.3-32.2-12.3-44.5 0L9.2 111.5c-12.3 12.3-12.3 32.2 0 44.5L109.3 256 9.2 356.1c-12.3 12.3-12.3 32.2 0 44.5l22.2 22.2c12.3 12.3 32.2 12.3 44.5 0L176 322.7l100.1 100.1c12.3 12.3 32.2 12.3 44.5 0l22.2-22.2c12.3-12.3 12.3-32.2 0-44.5L242.7 256z"
-      />
-    </svg>
-    <span>FOLD</span>
-  </div>
+  </transition>
+
+  <transition name="fold">
+    <div v-if="isOpen" class="fold" @click="setIsOpen(false)">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="12"
+        height="12"
+        viewBox="0 0 352 512"
+      >
+        <path
+          d="M242.7 256l100.1-100.1c12.3-12.3 12.3-32.2 0-44.5l-22.2-22.2c-12.3-12.3-32.2-12.3-44.5 0L176 189.3 75.9 89.2c-12.3-12.3-32.2-12.3-44.5 0L9.2 111.5c-12.3 12.3-12.3 32.2 0 44.5L109.3 256 9.2 356.1c-12.3 12.3-12.3 32.2 0 44.5l22.2 22.2c12.3 12.3 32.2 12.3 44.5 0L176 322.7l100.1 100.1c12.3 12.3 32.2 12.3 44.5 0l22.2-22.2c12.3-12.3 12.3-32.2 0-44.5L242.7 256z"
+        />
+      </svg>
+      <span>FOLD</span>
+    </div>
+  </transition>
 </template>
 
 <script setup lang="ts">
@@ -121,6 +106,21 @@ defineProps<{
   border: solid 1px rgba(0, 0, 0, 0.25);
 }
 
+.children-enter-to,
+.children-leave-from {
+  transform: scaleY(1);
+}
+
+.children-enter-active,
+.children-leave-active {
+  transition: transform 200ms;
+}
+
+.children-enter-from,
+.children-leave-to {
+  transform: scaleY(0);
+}
+
 .children {
   padding: 0.75rem;
   border: solid 1px rgba(0, 0, 0, 0.25);
@@ -134,6 +134,21 @@ defineProps<{
     transition: opacity 0.2s ${isOpen ? '0.2s' : '0s'};
     opacity: ${isOpen ? 1 : 0};
   } */
+}
+
+.fold-enter-to,
+.fold-leave-from {
+  opacity: 1;
+}
+
+.fold-enter-active,
+.fold-leave-active {
+  transition: opacity 200ms;
+}
+
+.fold-enter-from,
+.fold-leave-to {
+  opacity: 0;
 }
 
 .fold {
