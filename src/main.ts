b991b7711d51389e9ac722f9713f338364d53dee
shirayuki <shirayuki@46ki75.com>
2024-08-14 00:32:46 +0900

    fix: export everything from index

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import 'prismjs'
 import './style.scss'
 import App from './App.vue'
 
-import { ElmethisPlugin } from './plugins/ElmethisPlugin'
+import { ElmethisPlugin } from '.'
 
 const app = createApp(App)
 app.use(ElmethisPlugin)
